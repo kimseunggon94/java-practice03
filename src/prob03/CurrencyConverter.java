7b3228f0f86ec3de1570c95e41647366122748c2
BIT <BIT@DESKTOP-TJ8UB7F>
2019-08-08 16:13:16 +0900

    문제3

--- src/prob03/CurrencyConverter.java
+++ src/prob03/CurrencyConverter.java
@@ -1,4 +1,16 @@
 package prob03;
 
 public class CurrencyConverter {
+	private static double rate;
+	
+	public static double toDollar(double won) {
+		return won/rate;
+	}
+	public static double toKRW(double dollar) {
+		return rate*dollar;
+	}
+	public static void setRate(double rate) {
+		CurrencyConverter.rate=rate;			//클래스 이름으로 접근해야함
+	}
+	
 }
