d4dbe4ada4c47c8464e38373b430432afb4d07a2
BIT <BIT@DESKTOP-TJ8UB7F>
2019-08-08 16:14:31 +0900

    문제6

---
+++ src/prob06/Add.java
@@ -0,0 +1,12 @@
+package prob06;
+
+public class Add {
+	int a,b;
+	public void setValue(int a, int b) {
+		this.a=a;
+		this.b=b;
+	}
+	int calculate() {
+		return a+b;
+	}
+}
--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -6,34 +6,56 @@ public class CalcApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		while( true ) {
 
 			System.out.print( ">> " );
 			String expression = scanner.nextLine();
-			
+
 			if( "quit".equals( expression ) ) {
 				break;
 			}
-			
-			String[] tokens = expression.split( " " );
-			
+
+			String[] tokens = expression.split(" ");
+
+
 			if( tokens.length != 3 ) {
 				System.out.println( ">> 알 수 없는 식입니다.");
 				continue;
 			}
-			
 
-			
-			/*  코드를 완성 합니다 */
-			
-			
-			
-			
+			switch(tokens[1].toString()) {
+			case "+" : 
+				Add add=new Add();
+				add.setValue(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[2]));
+				System.out.println(">> "+add.calculate()); 
+				break;
+			case "-" : 
+				Sub sub=new Sub();
+				sub.setValue(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[2]));
+				System.out.println(">> "+sub.calculate());
+				break;
+			case "*" :
+				Mul mul=new Mul();
+				mul.setValue(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[2]));
+				System.out.println(">> "+mul.calculate());
+				break;
+			case "/" :
+				Div div=new Div();
+				div.setValue(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[2]));
+				System.out.println(">> "+div.calculate());
+				break;
+			case "default" : 
+				System.out.println("잘못된 입력입니다.");
+			}
+
+
+
 		}
-		
+
 		scanner.close();
 
 	}
 
 }
+
